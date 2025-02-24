9d312c3c881bc325cd4110f0456891cc1ef4e264
HoangDucBach <hoangbach0985@gmail.com>
2025-02-24 16:16:56 +0700

    add(items): interface for company

---
+++ contracts/core/player/Company.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "../items/interfaces/IERC721Company.sol";
+import "../../core/libraries/PlayerLibrary.sol";
+
+contract Company is IERC721Company {
+}
