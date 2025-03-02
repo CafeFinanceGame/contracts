7f11f87823bde5f7c9c5ac939766b438e42a1061
HoangDucBach <hoangbach0985@gmail.com>
2025-03-02 20:05:19 +0700

    fix: global interfaces

--- contracts/tokenomics/CAFPoolFactory.sol
+++ contracts/tokenomics/CAFPoolFactory.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-import "./interfaces/ICAFPoolFactory.sol";
+import "../interfaces/ICAFPoolFactory.sol";
 import "./CAFPool.sol";
 
 contract CAFPoolFactory is ICAFPoolFactory {
